f0e6434daa8a7168f0757257d5972cffec2e1a64
Rafael Lopez <lpz.raf@gmail.com>
2018-06-10 23:32:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name) ;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name) ;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name) ;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(name);
+}
+
+function appendKitten(name) {
+  kittens.push(kittens);
+}
